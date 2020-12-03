b8bae553a05bcf76d2537294bcacc8823a264b4e
kbritt510 <kbritt510@gmail.com>
2020-12-03 01:04:27 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+sayHello = () => <h1>Hello everyone!</h1>
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.sayHello} />
   </div>,
   document.getElementById('root')
 );
